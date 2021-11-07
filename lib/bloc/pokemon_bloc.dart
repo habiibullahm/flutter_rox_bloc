d7cfd1aa86a3dd2d4f9d9d78159fd50f9c1d9d5e
habibullahm <muhammadhabibullah32@gmail.com>
2021-11-08 01:47:35 +0700

    show list

---
+++ lib/bloc/pokemon_bloc.dart
@@ -0,0 +1,27 @@
+import '../pokemon_repository.dart';
+import 'pokemon_event.dart';
+import 'pokemon_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class PokemonBloc extends Bloc<PokemonEvent, PokemonState> {
+  final _pokemonRepository = PokemonRepository();
+
+  PokemonBloc() : super(PokemonInitial());
+
+  @override
+  Stream<PokemonState> mapEventToState(PokemonEvent event) async* {
+    if (event is PokemonPageRequest) {
+      yield PokemonLoadInProgress();
+
+      try {
+        final pokemonPageResponse =
+            await _pokemonRepository.getPokemonPage(event.page);
+        yield PokemonPageLoadSuccess(
+            pokemonListings: pokemonPageResponse.pokemonListings,
+            canLoadNextPage: pokemonPageResponse.canLoadNextPage);
+      } catch (e) {
+        yield PokemonPageLoadFailed(error: e);
+      }
+    }
+  }
+}
